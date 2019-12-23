4e3812f2e567b621cc5cf7c3d2f561682997a744
JayL <liujianping.itech@qq.com>
2019-12-23 18:39:50 +0800

    update annotate fmt

--- annotate.go
+++ annotate.go
@@ -27,7 +27,7 @@ func Annotatef(err error, format string, args ...interface{}) error {
 
 //Error annotator implemention
 func (err *annotator) Error() string {
-	return fmt.Sprintf("%s: %s", err.annotation, err.err.Error())
+	return fmt.Sprintf("%s %s", err.annotation, err.err.Error())
 }
 
 //Cause annotator implemention
